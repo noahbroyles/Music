69638e0c47416943872c2e1769e25c292ce9ad41
Noah Broyles <noah@javamate.net>
2020-03-27 08:39:55 -0400

    Initialized repo

---
+++ ytUrl.py
@@ -0,0 +1,28 @@
+import requests
+import sys
+from bs4 import BeautifulSoup
+
+
+def urlFromQuery(query):
+    query = query.replace(' ', '+')
+    try:
+        page = requests.get(f'https://www.youtube.com/results?search_query={query}').text
+    except requests.exceptions.ConnectionError:
+        print("There was an error connecting to YouTube. Check Internet/Proxy settings.")
+        sys.exit()
+    soup = BeautifulSoup(page, 'lxml')
+    results = soup.find_all('a', {'class': 'yt-uix-tile-link'})
+    if len(results) == 0:
+        return None
+    else:
+        for vid in results:
+            if '/watch' in vid['href']:
+                url = 'https://www.youtube.com' + vid['href']
+    return url
+
+
+if __name__ == "__main__":
+    search = input("What do you want to find a link to on YouTube? ")
+    url = urlFromQuery(search)
+    if url is not None:
+        print(url, "is your url")
