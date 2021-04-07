fc403d734d45af46c7691c43a9b3cb11c5068645
Noah Broyles <noah@javamate.net>
2021-04-07 10:07:51 -0400

    Smart queue command, added command options

--- youtubeStream/__main__.py
+++ youtubeStream/__main__.py
@@ -4,11 +4,20 @@ import sys
 
 import vlc
 import pafy
+import json
 import random
 import tubehelper
 from termcolor import colored
 
 print(colored("YOUTUBE MUSIC STREAMER ROARING TO LIFE...", "blue"))
+
+
+actions = colored("play", "green") + "        > play song from url or search\n" \
+          + colored("playls", "green") + "     > play a YouTube or local playlist in order\n" \
+          + colored("shuffle", "green") + "     > shuffle a YouTube or local playlist" + \
+          + colored("makepls", "green") + "     > make a new local playlist\n" \
+          + colored("editpls", "green") + "     > edit an existing local playlist" \
+          + colored("exit", "green") + "        > exit the player\n"
 
 # creating vlc media player
 instance = vlc.Instance("--no-video")
@@ -64,7 +73,19 @@ def queue(url=None, songTitle=None):
 
 # TODO: Add an actual main method. Right it just shuffles from a playlist.
 def main():
-    pass
+    while True:
+        print()
+        print('What would you like to do? ("' + colored('show', "yellow") + '" to show commands): ')
+        action = input("youtube-streamer ~ $ ")
+
+        if action == "show":
+            print("\n" + actions)
+
+        elif action == "playls":
+            local = input("YouTube or local playlist? [Y/l] ").lower()[0]
+            if local == "y" or local == "":
+                # We be playin' a YouTube PlayList
+                pass
 
 
 def playStream(url):
@@ -91,23 +112,30 @@ def playStream(url):
             else:
                 print()  # For beauty
                 break  # Quit the command handling loop
-            do = input().lower()
+            do = input().lower().strip()
             if do == "time":
                 print(colored(songTime(player.get_time() / 1000), "blue"))
             elif do == "pause":
                 player.pause()
             elif do == "play":
                 player.play()
-            elif do == "queue":
-                try:
-                    songToQueue = input("Enter the name or URL for the song you wish to queue: ").strip()
-                    if songToQueue.strip().startswith("https://"):
-                        # I guess it's a URL my dude
-                        queue(url=songToQueue)
-                    else:
-                        queue(songTitle=songToQueue)
-                except KeyboardInterrupt:
-                    pass
+            elif do:
+                pass
+            elif do.startswith("queue") or do.startswith("q"):
+                if " " in do:
+                    # They told us what to queue already
+                    songToQueue = " ".join(do.split(" ")[1:])
+                else:
+                    # Gotta ask 'em what they wanna hear
+                    try:
+                        songToQueue = input("Enter the name or URL for the song you wish to queue: ").strip()
+                        if songToQueue.strip().startswith("https://"):
+                            # I guess it's a URL my dude
+                            queue(url=songToQueue)
+                        else:
+                            queue(songTitle=songToQueue)
+                    except KeyboardInterrupt:
+                        pass
             elif do == "stop" or do == "skip":
                 player.stop()
                 return
