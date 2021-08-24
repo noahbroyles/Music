6438fea3db865b72222be6e2d489342bdccf63b9
Noah Broyles <noah@javamate.net>
2021-08-24 19:20:57 -0400

    Update to match site changes

--- myFreeMp3/scraper.py
+++ myFreeMp3/scraper.py
@@ -1,11 +1,14 @@
+# Scrapes the myfreemp3 site with selenium to download Mp3 songs.
+# Last confirmed working: 8/24/2021 @ 7:20 PM
+
 import time
 import requests
 from os import path
 
 from selenium import webdriver
 from selenium.webdriver import ChromeOptions
+from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.firefox.options import Options as FOptions
 
 browserName = 'chrome'
@@ -34,19 +37,23 @@ def downloadSong(songTitle: str):
         options = ChromeOptions()
         options.add_argument("--headless")
         browser = webdriver.Chrome(executable_path=f'{homeDir}/.drivers/chromedriver', options=options) if path.exists(f"{homeDir}/.drivers/chromedriver") else webdriver.Chrome(options=options)
-    browser.get('https://myfreemp3.vip/')
-    actions = ActionChains(browser)
+    browser.get('https://myfreemp3juices.cc/')
 
     # Search for song
     searchBar = browser.find_element_by_id('query')
     searchBar.clear()
     searchBar.send_keys(songTitle)
+    searchBar.send_keys(Keys.ENTER)
     searchButton = browser.find_element_by_xpath("/html/body/div[2]/div[1]/div/span/button")
-    actions.click(searchButton).perform()
+    # Click the search button as well as pushing enter
+    # for i in range(2):
+    # searchButton.click()
 
     # Wait until page loads
     def readyToProceed():
         try:
+            # /html/body/div[2]/div[2]/div[2]/li[1]/a[1]
+            searchButton.click()
             _ = browser.find_element_by_xpath('//*[@id="result"]/div[2]/li[1]/a[1]')
         except NoSuchElementException:
             return False
@@ -55,10 +62,16 @@ def downloadSong(songTitle: str):
 
     # Click download button
     songID = browser.find_element_by_xpath('//*[@id="result"]/div[2]/li[1]/a[1]').get_attribute('data-src').split('/')[-1]
-    downloadLink = f"https://free.mp3-download.best/{songID}"
+    downloadLink = f"https://speed.idmp3s.com/{songID}"
     browser.quit()
 
     # Download the song and save it
     r = requests.get(downloadLink)
     with open(f'{CamelCase(songTitle)}.mp3', 'wb') as f:
         f.write(r.content)
+
+
+# Main for testing
+if __name__ == "__main__":
+    browserName = 'firefox'
+    downloadSong('Yeah!')
